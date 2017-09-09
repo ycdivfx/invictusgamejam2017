71dce3607291c0484d4b98cd2ecac3fb9bece011
Pedro Branquinho <pedrobranquinho@hotmail.com>
2017-09-09 17:42:50 +0100

    Sound Manager + basic Sound Menu.

--- ZZ-Bottom/Assets/Scripts/SoundManager.cs
+++ ZZ-Bottom/Assets/Scripts/SoundManager.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : Singleton<SoundManager>
 {
+    [Header("Settings")]
+    private bool music = true;
+    private bool sfx = true;
+
+    [Header("Audio Management UI Elements")]
     public AudioSource m_audioSourceSFX;
+    public Toggle toggleSFX;
+    public Slider sliderSFX;
+
     public AudioSource m_audioSourceMusic;
+    public Toggle toggleMusic;
+    public Slider sliderMusic;
+
+    [Header("Sounds")]
     public AudioClip Music;
     public AudioClip Shoot;
     public AudioClip HitChar;
@@ -23,18 +36,32 @@ public class SoundManager : Singleton<SoundManager>
     public void PlaySfx(AudioClip clip)
     {
         m_audioSourceSFX.clip = clip;
-        m_audioSourceSFX.Play();
+        if (sfx)
+            m_audioSourceSFX.Play();
     }
-
-    public void PlayMusic()
+    public void PlayMusic(AudioClip clip)
     {
-        m_audioSourceMusic.clip = Music;
+        m_audioSourceMusic.clip = clip;
         m_audioSourceMusic.loop = true;
-        m_audioSourceMusic.Play();
+        if (music)
+            m_audioSourceMusic.Play();
     }
 
-    public void StopMusic()
+    public void MuteSFX()
     {
-        m_audioSourceMusic.Stop();
+        m_audioSourceSFX.mute = !m_audioSourceSFX.mute;
+    }
+    public void MuteMusic()
+    {
+        m_audioSourceMusic.mute = !m_audioSourceMusic.mute;
+    }
+
+    public void VolumeSFX()
+    {
+        m_audioSourceSFX.volume = sliderSFX.value;
+    }
+    public void VolumeMusic()
+    {
+        m_audioSourceMusic.volume = sliderMusic.value;
     }
 }
