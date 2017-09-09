76cd25eb0832d87179bee4ee4ccf8526c9633585
Daniel Santana <danielgsantana@gmail.com>
2017-09-09 09:53:22 +0100

    Change velocity on component for player

--- ZZ-Bottom/Assets/Scripts/PlayerWeapon.cs
+++ ZZ-Bottom/Assets/Scripts/PlayerWeapon.cs
@@ -6,8 +6,19 @@ public class PlayerWeapon : MonoBehaviour
 {
     public LuckyBullet LuckyBulletObject;
     public CrazyBullet CrazyBulletObject;
+    public Vector2 BulletStartOffset;
 
     public float Distance;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+
+
 
     public void Shoot()
     {
