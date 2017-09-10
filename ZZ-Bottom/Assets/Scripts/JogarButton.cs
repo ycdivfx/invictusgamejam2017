411ca4339f2bf32ef5399cd0f112d016ecef6fd8
Llebo <llebo94@gmail.com>
2017-09-10 02:17:53 +0100

    Scene Menu
    Scene Menu Creation
    Arbutus Font Added

---
+++ ZZ-Bottom/Assets/Scripts/JogarButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JogarButton : MonoBehaviour
+{
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("level_01");
+    }
+}
