4a226dd26b21bf5b19c008bc2fe0bfb391b811a7
Daniel Santana <danielgsantana@gmail.com>
2017-09-10 10:49:25 +0100

    DemoMode

---
+++ ZZ-Bottom/Assets/Scripts/DemoMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var nextLevel = Input.GetKeyUp(KeyCode.Alpha1);
+	    var god = Input.GetKeyUp(KeyCode.Alpha0);
+        if (nextLevel) GameManager.Instance.NextLevel();
+	}
+}
